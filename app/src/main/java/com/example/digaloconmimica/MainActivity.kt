7d48d6257457e7cc62dc3b2b9f468491545649aa
Folis455 <62319136+Folis455@users.noreply.github.com>
2023-06-11 04:21:37 -0300

    0.2

--- app/src/main/java/com/example/digaloconmimica/MainActivity.kt
+++ app/src/main/java/com/example/digaloconmimica/MainActivity.kt
@@ -2,14 +2,17 @@ package com.example.digaloconmimica
 
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     private val movies = listOf("Movie 1", "Movie 2", "Movie 3", "Movie 4")
+    private var countDownTimer: CountDownTimer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,18 +21,25 @@ class MainActivity : AppCompatActivity() {
         val movieTitleTextView: TextView = findViewById(R.id.movie_title_text_view)
         val generateMovieButton: Button = findViewById(R.id.generate_movie_button)
         val timerDurationEditText: EditText = findViewById(R.id.timer_duration_edit_text)
-        val startTimerButton: Button = findViewById(R.id.start_timer_button)
         val timerTextView: TextView = findViewById(R.id.timer_text_view)
+        val stopTimerButton: Button = findViewById(R.id.stop_timer_button)
 
         generateMovieButton.setOnClickListener {
             val randomMovie = getRandomMovie(movies)
             movieTitleTextView.text = randomMovie
+            val durationInSeconds = timerDurationEditText.text.toString().toLongOrNull()
+            if (durationInSeconds != null) {
+                val durationInMillis = durationInSeconds * 1000
+                countDownTimer?.cancel()
+                countDownTimer = startTimer(durationInMillis, timerTextView)
+            } else {
+                Toast.makeText(this, "Please enter a valid time", Toast.LENGTH_SHORT).show()
+            }
         }
 
-        startTimerButton.setOnClickListener {
-            val durationInSeconds = timerDurationEditText.text.toString().toLong()
-            val durationInMillis = durationInSeconds * 1000
-            startTimer(durationInMillis, timerTextView)
+        stopTimerButton.setOnClickListener {
+            countDownTimer?.cancel()
+            timerTextView.text = "Timer stopped"
         }
     }
 
@@ -38,10 +48,15 @@ class MainActivity : AppCompatActivity() {
         return movies[randomIndex]
     }
 
-    private fun startTimer(durationInMillis: Long, timerTextView: TextView) {
-        object : CountDownTimer(durationInMillis, 1000) {
+    private fun startTimer(
+        durationInMillis: Long,
+        timerTextView: TextView
+    ): CountDownTimer {
+        return object : CountDownTimer(durationInMillis, 1000) {
             override fun onTick(millisUntilFinished: Long) {
-                timerTextView.text = (millisUntilFinished / 1000).toString()
+                val minutes = (millisUntilFinished / 1000) / 60
+                val seconds = (millisUntilFinished / 1000) % 60
+                timerTextView.text = String.format("%02d:%02d", minutes, seconds)
             }
 
             override fun onFinish() {
@@ -50,3 +65,8 @@ class MainActivity : AppCompatActivity() {
         }.start()
     }
 }
+
+
+
+
+
